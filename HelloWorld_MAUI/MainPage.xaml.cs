a7df89c90bcb7290050d5f0d9353d33992f7ffc3
Evgenia <evgenia.noasova1002@gmail.com>
2025-10-12 03:45:00 +0300

    Task 1.1 - Hello World in Console, WinForms, WPF, and MAUI

---
+++ HelloWorld_MAUI/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace MauiApp4;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnConfirmClicked(object sender, EventArgs e)
+    {
+        if (int.TryParse(numberEntry.Text, out int n))
+        {
+            DisplayAlert("Result", $"You entered the number {n}", "OK");
+        }
+        else
+        {
+            DisplayAlert("Error", "Please enter a valid integer!", "OK");
+        }
+    }
+}
