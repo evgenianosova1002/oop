2f6e985db0e58f34ccd57e453d442551a5b6c08a
Evgenia <evgenia.noasova1002@gmail.com>
2025-10-13 08:57:31 +0300

    Task 1.2 – Input Number (Console + MAUI)

---
+++ Project1.2/ConsoleApp4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter an integer: ");
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int number))
+        {
+            Console.WriteLine($"You entered the number {number}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input! Please enter a valid integer.");
+        }
+    }
+}
+
---
+++ Project1.2/MauiApp5/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace MauiApp5;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnConfirmClicked(object sender, EventArgs e)
+    {
+        if (int.TryParse(numberEntry.Text, out int n))
+        {
+            DisplayAlert("Result", $"You entered number {n}", "OK");
+        }
+        else
+        {
+            DisplayAlert("Error", "Please enter a valid integer!", "OK");
+        }
+    }
+}
